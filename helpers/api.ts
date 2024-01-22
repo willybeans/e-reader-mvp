379878a206ccb05f0d58b085ab9e7fc166e04af3
Will Wedmedyk <willwedmedyk@gmail.com>
2024-01-22 17:58:17 -0500

    ww/add api logic + device checking

--- helpers/api.ts
+++ helpers/api.ts
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 export type ReqBody = {
   method: "POST" | "GET";
   body: any;
@@ -6,11 +8,23 @@ export type ReqBody = {
   };
 };
 
+export const buildUrl = (): string => {
+  return `http://${getLocalType()}:8080`;
+};
+
+export const getLocalType = (): string =>
+  Platform.OS === "android" ? "10.0.2.2" : "localhost";
+
 export function api<T>(url: string, reqBody?: ReqBody): Promise<T> {
-  return fetch(url, reqBody).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json() as Promise<T>;
-  });
+  return fetch(url, reqBody)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json() as Promise<T>;
+    })
+    .catch((e) => {
+      console.error("problem with fetch: ", e);
+      throw e;
+    });
 }

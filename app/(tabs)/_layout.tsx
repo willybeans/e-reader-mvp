1ac5f709d81d9d6ebf14dae5b350f09b2426a7ba
Will Wedmedyk <willwedmedyk@gmail.com>
2024-02-01 09:34:54 -0500

    ww/nav: adding nav icon logic

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
 
-import Colors from '../../constants/Colors';
+import Colors from "../../constants/Colors";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -19,14 +19,44 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: React.ComponentProps<typeof FontAwesome>["name"] =
+            "code";
+          switch (route.name) {
+            case "chats":
+              // iconName = "home";
+              iconName = "wechat";
+              break;
+            case "learn":
+              iconName = "pencil";
+              break;
+            case "explore":
+              iconName = "globe";
+              break;
+            case "profile":
+              iconName = "user";
+              break;
+            default:
+              break;
+          }
+          return <TabBarIcon name={iconName} color={color} />;
+        },
+        // headerTintColor: grayDark.gray11,
+        // headerStyle: {
+        //   backgroundColor: "black",
+        // },
+        // headerTitleAlign: "center",
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        // tabBarInactiveTintColor: grayDark.gray11,
+
+        tabBarStyle: { backgroundColor: "black" },
+      })}
+    >
       <Tabs.Screen
-        name="index"
+        name="chats"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Chats",
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -34,7 +64,7 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -44,10 +74,21 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="learn"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Learn",
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
         }}
       />
     </Tabs>

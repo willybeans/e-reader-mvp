7d4bc4d309d88ecf7dabf62c1756783ae1da25c3
Will Wedmedyk <willwedmedyk@gmail.com>
2024-02-12 09:29:43 -0500

    ww/websockets: cleaning

--- context/WebSocket/useWebSocketContext.tsx
+++ context/WebSocket/useWebSocketContext.tsx
@@ -1,22 +1,6 @@
 import { useContext, useEffect } from "react";
 import Context from "./Context";
 
-// export const useWebSocketContext = () => {
-//   const { isReady, val, send } = useContext(Context); // use it just like a hook
-
-//   useEffect(() => {
-//     if (isReady) {
-//       send("test message");
-//     }
-//   }, [isReady, send]); // make sure to include send in dependency array
-
-//   return (
-//     <div>
-//       Ready: {JSON.stringify(isReady)}, Value: {val}
-//     </div>
-//   );
-// };
-
 const useWebSocketContext = () => {
   const webSocketContext = useContext(Context);
   return webSocketContext;

a77561d0c69dcac2dbba16b29cab80bd66241b70
Will Wedmedyk <willwedmedyk@gmail.com>
2024-02-29 15:55:24 -0500

    ww/connect: move chat state to context

--- context/WebSocket/Provider.tsx
+++ context/WebSocket/Provider.tsx
@@ -1,17 +1,49 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useReducer } from "react";
 import Context from "./Context";
-import { Websocket } from "./types";
+import {
+  Websocket,
+  Action,
+  ActionTypes,
+  ChatRoom,
+  Message,
+  WebSocketResponse,
+} from "./types";
+
+function reducer(state: ChatRoom[], action: Action): ChatRoom[] {
+  switch (action.type) {
+    case "get_messages": {
+      return [...state, ...action.content];
+    }
+    case "post_message": {
+      const indexOfChat = state.findIndex(
+        (x) => x.chat_room_id === action.content.chat_room_id
+      );
+      const stateCopy = [...state];
+      stateCopy[indexOfChat]?.chat_messages?.push(action.content);
+      return [...state, ...stateCopy];
+    }
+    default:
+      throw Error("Unknown action: " + action.type);
+  }
+}
+
+const initialState: ChatRoom[] = [];
 
 export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const [chatState, dispatch] = useReducer<
+    (chatState: ChatRoom[], actions: Action) => ChatRoom[]
+  >(reducer, initialState);
+
   const [isReady, setIsReady] = useState(false);
   const [val, setVal] = useState(null);
 
   const wsRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket("wss://echo.websocket.events/");
+    const testId = "d2792a62-86a4-4c49-a909-b1e762c683a3";
+    const ws = new WebSocket(`ws://localhost:8080/ws?user_id=${testId}`);
 
     ws.onopen = () => {
       setIsReady(true);
@@ -19,7 +51,20 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
     ws.onclose = () => {
       setIsReady(false);
     };
-    ws.onmessage = (event) => setVal(event.data);
+    ws.onmessage = (event) => {
+      const { data } = event;
+
+      let parsed = {} as WebSocketResponse;
+      try {
+        parsed = JSON.parse(data);
+        dispatch({
+          type: parsed.action,
+          content: parsed.content,
+        });
+      } catch (e) {
+        console.error("Failure to parse res obj: ", e);
+      }
+    };
 
     wsRef.current = ws;
 
@@ -28,10 +73,22 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
     };
   }, []);
 
+  const handleReducer = (
+    actionType: ActionTypes,
+    content: ChatRoom[] | Message
+  ) => {
+    dispatch({
+      type: actionType,
+      content: content,
+    });
+  };
+
   const ret = {
     isReady,
     val,
     send: wsRef.current?.send.bind(wsRef.current),
+    handleReducer,
+    chatState,
   } as Websocket;
 
   return <Context.Provider value={ret}>{children}</Context.Provider>;
--- context/WebSocket/types.ts
+++ context/WebSocket/types.ts
@@ -2,6 +2,7 @@ export interface Websocket {
   isReady: boolean;
   val: any | null;
   send: WebSocketSend;
+  chatState: ChatRoom[];
 }
 
 export type WebSocketSend = (
@@ -19,3 +20,37 @@ export type WebSocketContext = [
   receivedMessages: MessageBody[],
   send: WebSocketSend | undefined
 ];
+
+export interface Message {
+  id: string;
+  chat_room_id?: string;
+  user_id: string;
+  content: string;
+  sent_at: string;
+  sender_username?: string;
+}
+
+export interface ChatRoom {
+  chat_room_id: string;
+  chat_name: string;
+  usernames: string[];
+  chat_messages: Message[];
+}
+
+export interface ChatContext {
+  handleReducer: (action: ActionTypes, content: any) => void;
+  state: ChatRoom[];
+}
+
+export type ActionTypes = "get_messages" | "post_message";
+
+export type Action = {
+  type: ActionTypes;
+  content: any;
+};
+
+export type WebSocketResponse = {
+  subscription_id: string;
+  action: ActionTypes;
+  content: ChatRoom[];
+};

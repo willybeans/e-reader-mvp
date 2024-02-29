6dc08407165f6013fb6be619730decfab824993d
Will Wedmedyk <willwedmedyk@gmail.com>
2024-02-28 22:48:57 -0500

    ww/connect: chat provider

--- app/(tabs)/chats/_layout.tsx
+++ app/(tabs)/chats/_layout.tsx
@@ -1,26 +1,29 @@
 import { Stack } from "expo-router";
 import { Header } from "../../../components/Header";
+import { ChatProvider } from "../../../context/Chats";
 
 export default function ChatsLayout() {
   return (
-    <Stack
-      screenOptions={({ route }) => ({
-        // headerTintColor: grayDark.gray11,
-        headerStyle: {
-          backgroundColor: "black",
-        },
-        headerTitleAlign: "center",
-        // tabBarActiveTintColor: purple.purple8,
-        // tabBarInactiveTintColor: grayDark.gray11,
-        tabBarStyle: { backgroundColor: "black" },
-      })}
-    >
-      <Stack.Screen name="index" options={{ headerTitle: "Your Chats" }} />
-      <Stack.Screen
-        name="[id]"
-        // pass in users name
-        options={{ headerTitle: (props) => <Header {...props} /> }}
-      />
-    </Stack>
+    <ChatProvider>
+      <Stack
+        screenOptions={({ route }) => ({
+          // headerTintColor: grayDark.gray11,
+          headerStyle: {
+            backgroundColor: "black",
+          },
+          headerTitleAlign: "center",
+          // tabBarActiveTintColor: purple.purple8,
+          // tabBarInactiveTintColor: grayDark.gray11,
+          tabBarStyle: { backgroundColor: "black" },
+        })}
+      >
+        <Stack.Screen name="index" options={{ headerTitle: "Your Chats" }} />
+        <Stack.Screen
+          name="[id]"
+          // pass in users name
+          options={{ headerTitle: (props) => <Header {...props} /> }}
+        />
+      </Stack>
+    </ChatProvider>
   );
 }

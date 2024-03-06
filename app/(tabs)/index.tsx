657ae9567263acfd16dd535ede76e7819d3b8e47
Will Wedmedyk <willwedmedyk@gmail.com>
2024-03-06 11:36:33 -0500

    ww/content_fixes: cleaning index screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,12 @@
-import {
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  useColorScheme,
-} from "react-native";
-import { Text, View } from "../../components/Themed";
-import { Link, router } from "expo-router";
+import { ScrollView, StyleSheet, useColorScheme } from "react-native";
+import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { api, buildUrl } from "../../helpers/api";
+import { Text, View } from "../../components/Themed";
+
 import { ContentIcon } from "../../components/ContentIcon";
-import { FontAwesome } from "@expo/vector-icons";
-import Colors from "../../constants/Colors";
 import { Button } from "../../components/Button";
+
+import { api, buildUrl } from "../../helpers/api";
 
 export type Content = {
   id: string;
@@ -31,7 +26,6 @@ export default function LearnScreen() {
       const usersContent: Content[] = await api(
         `${buildUrl()}/getAllUserContent?id=${"d2792a62-86a4-4c49-a909-b1e762c683a3"}`
       );
-
       setContentList(usersContent);
     })();
   }, []);
@@ -55,26 +49,12 @@ export default function LearnScreen() {
     </View>
   ) : (
     <View style={styles.container}>
-      <Link href="/upload" asChild>
-        <Pressable style={{ alignItems: "center" }}>
-          <Text>Add Content</Text>
-          <FontAwesome
-            name="plus"
-            size={25}
-            color={Colors[colorScheme ?? "light"].text}
-            // style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-          />
-        </Pressable>
-      </Link>
-      <ScrollView style={{ width: "100%", height: "100%" }}>
-        <View>
-          <Text>Your Saved Content</Text>
-          <View
-            style={styles.separator}
-            lightColor="#eee"
-            darkColor="rgba(255,255,255,0.1)"
-          />
-        </View>
+      <ScrollView
+        horizontal={false}
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+      >
         <View style={styles.contentContainer}>
           {contentList?.map((c, i) => (
             <ContentIcon content={c} key={i} />
@@ -88,9 +68,12 @@ export default function LearnScreen() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
-    // flex: 1,
     alignItems: "center",
     justifyContent: "space-between",
+    paddingLeft: 10,
+    paddingRight: 10,
+    height: "100%",
+    width: "100%",
   },
   emptyContainer: {
     alignItems: "center",
@@ -102,8 +85,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    height: "100%",
-    width: "100%",
+    flexWrap: "wrap",
+    maxWidth: "100%",
     marginLeft: 10,
     marginRight: 10,
   },

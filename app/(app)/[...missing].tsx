fbe30993baaf4433c9c1195a5fa781cc300dd9de
Will Wedmedyk <willwedmedyk@gmail.com>
2024-03-18 10:43:17 -0400

    ww/content_fixes: missing screen page added svg

--- app/(app)/[...missing].tsx
+++ app/(app)/[...missing].tsx
@@ -1,15 +1,25 @@
 import { Link, Stack } from "expo-router";
-import { StyleSheet } from "react-native";
+import { StyleSheet, useColorScheme } from "react-native";
 
 import { Text, View } from "../../components/Themed";
+import Colors from "../../constants/Colors";
+
+import HurtFox from "../../svgs/hurt_fox.svg";
 
 export default function NotFoundScreen() {
+  const colorScheme = useColorScheme();
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
         <Text style={styles.title}>This screen doesn't exist.</Text>
-
+        <View style={{ backgroundColor: "transparent", borderRadius: 25 }}>
+          <HurtFox
+            height={300}
+            width={300}
+            style={{ color: Colors[colorScheme ?? "light"].tint }}
+          />
+        </View>
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
